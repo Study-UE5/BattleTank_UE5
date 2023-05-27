8226e45c7159e9b3e4783e036100790482895950
WORK\mywor <qwastaras>
2023-05-27 18:54:39 +0100

    002 Start Fix

---
+++ BattleTank/Source/BattleTank/BattleTankGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleTankGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ABattleTankGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
